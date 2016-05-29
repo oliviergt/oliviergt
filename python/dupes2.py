fd621e77a0dd758c4ba42c220ee770237b255ff4
Olivier Glassey-Tranguyen <olivier.gl@ssey.net>
2016-05-28 18:33:52 -0700

    progress

--- python/dupes2.py
+++ python/dupes2.py
@@ -5,33 +5,47 @@ import md5
 import os
 import re
 import shutil
+import sqlite3
 import subprocess
 import sys
 import time
-import sqlite3
 
-MD5SUM = '/usr/bin/md5sum'
 
-class Status(object):
+MD5SUM = [
+  ['md5sum'],   # GNU-style
+  ['md5', '-r'] # BSD-style
+]
+
+
+class Console(object):
   def __init__(self):
     self.last_refresh_time = time.time()
-    self.last_line_length = 0
 
-  def Print(self, s):
-    spaces = ''
-    if len(s) < self.last_line_length:
-      spaces = ' ' * (self.last_line_length - len(s))
-    print '\r%s%s' % (s, spaces)
+  def Print(self, s=''):
+    print '\033[K%s' % s
     sys.stdout.flush()
-    self.last_line_length = 0
+    self.last_refresh_time = time.time()
+
+  def Error(self, s):
+    print '\033[K\033[91m%s\033[0m' % s
+    sys.stdout.flush()
+    self.last_refresh_time = time.time()
 
   def Flash(self, s):
-    spaces = ''
-    if len(s) < self.last_line_length:
-      spaces = ' ' * (self.last_line_length - len(s))
-    print '\r%s%s' % (s, spaces),
+    now = time.time()
+    if now < self.last_refresh_time + 1:
+      return
+    width = self.GetConsoleWidth()
+    if len(s) > width:
+      b1 = width / 2 - 1
+      b2 = b1 + 3 + len(s) - width
+      s = s[:b1] + '...' + s[b2:]
+    print '\033[K%s\r' % s,
     sys.stdout.flush()
-    self.last_line_length = len(s)
+    self.last_refresh_time = now
+
+  def GetConsoleWidth(self):
+    return int(os.popen('stty size', 'r').read().split()[1])
 
 
 class FileStats(object):
@@ -101,41 +115,64 @@ class FileStatsRepository(object):
     cursor.execute(
         'SELECT * FROM file_stats WHERE path=? and base_name=?',
         (path, base_name))
-    result = cursor.fetchone()
-    if not result:
+    row = cursor.fetchone()
+    if not row:
       return None
-    return FileStats(result[0], result[1], result[2], result[3], result[4])
+    return self.MakeFileStats(row)
+
+  def Lookup(self, md5hash, size):
+    cursor = self.connection.cursor()
+    cursor.execute(
+        'SELECT * FROM file_stats WHERE md5hash=? and size=?',
+        (md5hash, size))
+    result = []
+    for row in cursor.fetchall():
+      result.append(self.MakeFileStats(row))
+    return result
+
+  def MakeFileStats(self, row):
+    return FileStats(row[0], row[1], row[2], row[3], row[4])
 
 
 def Call(args):
-  process = subprocess.Popen(args, stdout=subprocess.PIPE)
-  return [line for line in process.stdout]
+  process = subprocess.Popen(
+      args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  return [line for line in process.stdout], [line for line in process.stderr]
 
 
-def HashFile(filename):
+def HashFile(filename, console):
   '''Returns md5 hash of the file, or None if there was an error.
   For example, the current user may not have permission to read the
   file.
   '''
-  output_lines = Call([MD5SUM, filename])
+  success = False
+  global MD5SUM
+  for md5sum_command in MD5SUM:
+    try:
+      output_lines, err_lines = Call(md5sum_command + [filename])
+      success = True
+      break
+    except OSError:
+      pass
+  if not success:
+    raise Exception('Did not find md5 command')
+  MD5SUM = [md5sum_command]
   if len(output_lines) != 1:
-    print 'Could not hash %s' % filename
+    err = err_lines[0].replace(filename, '').strip()
+    console.Error('Could not hash %s: %s' % (filename, err))
     return None
   tokens = output_lines[0].split(' ')
   if len(tokens) < 1:
-    print 'No space in output lines when hashing %s:\n%s' % (
-        filename, output_lines[0])
+    console.Print('No space in output lines when hashing %s:\n%s' % (
+        filename, output_lines[0]))
     return None
   return tokens[0]
 
 
+class TreeWalker(object):
 
-
-class Dupes(object):
-
-  def  __init__(self, repository, status):
-    self.repository = repository
-    self.status = status
+  def __init__(self, console):
+    self.console = console
     self.exclusion_patterns = [
         re.compile('/\.AppleDouble/'),
         re.compile('\.swp$'),
@@ -149,14 +186,88 @@ class Dupes(object):
         return True
     return False
 
+  def Utf8Decode(self, s):
+    try:
+      return s.decode('utf8')
+    except UnicodeDecodeError, e:
+      self.console.Print('Not an utf8 filename: %s' % repr(s))
+    return None
+
+  def MakeAcceptableFile(self, filename):
+    if os.path.islink(filename):
+      return None
+    filename = self.Utf8Decode(filename)
+    if not filename:
+      return None
+    if self.IsExcluded(filename):
+      return None
+    return filename
+
+  def Walk(self, paths, name, callback):
+    """Processes all files / directories recursively, and calls the callback on
+    each absolute filename.
+
+    Parameters
+    ----------
+    paths : list of str
+        Each entry can be a file or a directory. Directories will be explored
+        recusively and expanded into files.
+    name: str
+        Name of the operation, for the progress indicator.
+    callback: function
+        Function of one parameter (the absolute file name). Will be called on
+        each file.
+    """
+    paths = [os.path.abspath(os.path.expanduser(p)) for p in paths]
+    # First pass: Gather statistics on files and directories.
+    files = []
+    directories = set()
+    for path_argument in paths:
+      if not os.path.isdir(path_argument):
+        files.append(path_argument)  # TODO: Do the counting right here.
+      else:
+        for root, folders, regular_files in os.walk(path_argument, topdown=False):
+          for filename in regular_files:
+            filename = self.MakeAcceptableFile(os.path.join(root, filename))
+            if filename:
+              files.append(filename)
+              directories.add(root)
+              self.console.Flash('%s: %d files, %d directories: %s' % (
+                name, len(files), len(directories), root))
+
+    # Second pass: actual processing.
+    processed_directories = set()
+    file_count = 0
+    for path_argument in paths:
+      if not os.path.isdir(path_argument):
+        filename = self.MakeAcceptableFile(path_argument)
+        if filename:
+          callback(filename)  # TODO: Do the counting right here.
+      else:
+        for root, folders, regular_files in os.walk(path_argument, topdown=False):
+          for filename in regular_files:
+            filename = self.MakeAcceptableFile(os.path.join(root, filename))
+            if filename:
+              file_count += 1
+              self.console.Flash('%s: file %d/%d, directory %d/%d: %s' % (
+                name, file_count, len(files), len(processed_directories),
+                len(directories), root))
+              callback(filename)
+              processed_directories.add(root)
+
+
+class Dupes(object):
+
+  def  __init__(self, repository, tree_walker, console):
+    self.repository = repository
+    self.tree_walker = tree_walker
+    self.console = console
+
   def HashFileToDatabase(self, filename):
     """Retrieves timestamp and size from system. If those match the database
     values, the hash from the database is returned. Otherwise, the hash is
-    calculated. Returns the file stats object. Returns None if the file is
-    excluded or if the hash could not be computed."""
-    if self.IsExcluded(filename):
-      return None
-
+    calculated. Returns the file stats object. Returns None if the hash
+    could not be computed."""
     stat = os.stat(filename)
     timestamp_seconds = int(stat.st_mtime)
     size = stat.st_size
@@ -166,77 +277,41 @@ class Dupes(object):
       if (from_database.GetTimestampSeconds() == timestamp_seconds
           and from_database.GetSize() == size):
         return from_database
-    md5hash = HashFile(filename)
+    md5hash = HashFile(filename, self.console)
     if not md5hash:
       return None
     file_stats = FileStats(path, base_name, md5hash, size, timestamp_seconds)
     self.repository.Upsert(file_stats)
     return file_stats
 
-  def Utf8Decode(self, s):
-    try:
-      return s.decode('utf8')
-    except UnicodeDecodeError, e:
-      print 'Not an utf8 filename: %s' % repr(s)
-    return None
+  def HashPathsToDatabase(self, paths):
+    self.tree_walker.Walk(paths, 'hash_to_database', self.HashFileToDatabase)
 
-  def HashPathsToDatabase(self, hash_to_database):
-    hash_to_database = [os.path.abspath(os.path.expanduser(p))
-                        for p in args.hash_to_database]
+  def Lookup(self, paths):
+    self.tree_walker.Walk(paths, 'lookup', self.LookupFile)
 
-    # First pass: Gather statistics on files and directories.
-    files = []
-    directories = set()
-    for path_argument in hash_to_database:
-      if not os.path.isdir(path_argument):
-        files.append(path_argument)
-      else:
-        for root, folders, regular_files in os.walk(path_argument, topdown=False):
-          for filename in regular_files:
-            filename = os.path.join(root, filename)
-            if os.path.islink(filename):
-              continue
-            filename = self.Utf8Decode(filename)
-            if not filename:
-              continue
-            if not self.IsExcluded(filename):
-              files.append(filename)
-              directories.add(root)
-
-    self.status.Print('%s files in %s directories' % (len(files), len(directories)))
-
-    # Second pass: actual hashing.
-    hashed_directories = set()
-    file_count = 0
-    for path_argument in hash_to_database:
-      if not os.path.isdir(path_argument):
-        self.HashFileToDatabase(path_argument)
-      else:
-        for root, folders, regular_files in os.walk(path_argument, topdown=False):
-          for filename in regular_files:
-            filename = os.path.join(root, filename)
-            if os.path.islink(filename):
-              continue
-            filename = self.Utf8Decode(filename)
-            if not filename:
-              continue
-            file_count += 1
-            self.status.Flash('Hashing file %d, directory %d: %s' % (
-              file_count, len(hashed_directories), root))
-            self.HashFileToDatabase(filename)
-            hashed_directories.add(root)
+  def LookupFile(self, filename):
+    file_stats = self.HashFileToDatabase(filename)
+    matches = self.repository.Lookup(file_stats.GetHash(), file_stats.GetSize())
+    for other_file_stats in matches:
+      self.console.Print(os.path.join(
+        other_file_stats.GetPath(), other_file_stats.GetBaseName()))
+    self.console.Print()
 
 
 def Main(args):
-  status = Status()
+  console = Console()
   database = os.path.expanduser(args.database)
   repository = FileStatsRepository(database)
   repository.CreateTable()
-  dupes = Dupes(repository, status)
+  tree_walker = TreeWalker(console)
+  dupes = Dupes(repository, tree_walker, console)
   if args.hash_to_database:
     dupes.HashPathsToDatabase(args.hash_to_database)
+  if args.lookup:
+    dupes.Lookup(args.lookup)
   repository.Close()
-  print 'Updates saved to %s' % database
+  console.Print('Updates saved to %s' % database)
 
 
 if __name__ == "__main__":
@@ -249,6 +324,8 @@ if __name__ == "__main__":
   parser.add_argument('--hash_to_database', metavar='path', nargs='*',
       help='a search path that should be explored; hashes will be computed '
       'and added to the database')
+  parser.add_argument('--lookup', metavar='path', nargs='*',
+      help='a search path that should be explored; all files that match the '
+      'hashes and sizes from the search path will be returned')
   
-  args = parser.parse_args()
-  Main(args)
+  Main(parser.parse_args())
